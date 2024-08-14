09b5d19e19098c7398d65fa03ba5319c40941025
AniiiiKeT <127036480+AniiiiKeT@users.noreply.github.com>
2024-08-14 20:56:54 +0530

    This is the final draft of the movie genre classification project

--- Movie_Genre_Classification.py
+++ Movie_Genre_Classification.py
@@ -1,29 +1,56 @@
-#Custom Imports
-from Dataset_Creation import Dataset_Creation
+# Custom Imports
+from Dataset_Processor import DatasetProcessor
 
-#Library Imports
+# Library Imports
 import pandas as pd 
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import joblib
 
-#Instantiating the data object and getting the dataset
-data_object = Dataset_Creation()
-data = data_object.dataset_creator('Dataset/Genre Classification Dataset/train_data.txt')
-data = data_object.dataset_cleaning(data)
 
-#Initiating the model
-model = MultinomialNB()
-tfid_vectorizer = TfidfVectorizer()
-x = data['DESCRIPTION']
-y = data['GENRE']
+# Instantiating the dataset processor object and loading the dataset
+# processor = DatasetProcessor()
+# data = processor.load_dataset('Dataset/Genre Classification Dataset/train_data.txt')
+# cleaned_data = processor.clean_dataset(data)
 
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=42)
-x_train = tfid_vectorizer.fit_transform(x_train)
-x_test = tfid_vectorizer.transform(x_test)
+# cleaned_data.to_csv('Processed_Data.csv')
 
-model.fit(x_train,y_train)
-y_pred = model.predict(x_test)
-print(y_pred)
+# Setting up the model
+cleaned_data = pd.read_csv('Processed_Data.csv')
+
+# model = MultinomialNB()
+model = RandomForestClassifier()
+tfidf_vectorizer = TfidfVectorizer()
+
+# Splitting the dataset into features and labels
+X = cleaned_data['Description']
+y = cleaned_data['Genre']
+
+# Splitting the dataset into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Transforming the text data into TF-IDF features
+X_train = tfidf_vectorizer.fit_transform(X_train)
+X_test = tfidf_vectorizer.transform(X_test)
+
+
+# Training the model
+# model.fit(X_train, y_train)
+# joblib.dump(model,'Movie_Genre_Classification_RandomForest.pkl')
+
+model = joblib.load('Movie_Genre_Classification_RandomForest.pkl')
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test,y_pred)
+confusion_matrix = confusion_matrix(y_test,y_pred)
+
+print(accuracy)
+print(classification_report(y_test,y_pred))
+
+
+
