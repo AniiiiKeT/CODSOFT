e305601697ee6da3d20b4a328f82c193838b869c
Aniket Patil <127036480+AniiiiKeT@users.noreply.github.com>
2024-09-02 15:20:35 +0530

    Delete Setup.py

--- Setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name = "CODSOFT PROJECTS",
-    author = "Aniket Patil",
-    author_email = "aniket1972003@gmail.com"
-    packages = find_packages()
-)
